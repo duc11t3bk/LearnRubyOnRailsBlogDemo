f48e93669da270e6b03bae16e47267c6026dcd0a
MinhDuc <duc11t3bk@gmail.com>
2016-08-01 01:12:02 +0700

    Complete sign up view

--- BlogDemo/app/helpers/application_helper.rb
+++ BlogDemo/app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+	def full_title(page_title="")
+		base_title= "Ruby on Rails Blog Demo"
+		if page_title.empty?
+			return base_title	
+		else
+			return page_title + " | " + base_title
+		end
+	end
 end

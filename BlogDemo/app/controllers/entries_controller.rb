94568452282e2c49b007f46d6edb3ce0308cbf25
MinhDuc <duc11t3bk@gmail.com>
2016-08-02 04:31:45 +0700

    add home show entry for guess

--- BlogDemo/app/controllers/entries_controller.rb
+++ BlogDemo/app/controllers/entries_controller.rb
@@ -1,5 +1,5 @@
 class EntriesController < ApplicationController
-	before_action :logged_in_user
+	before_action :logged_in_user, only: [:new, :create, :destroy]
 	before_action :correct_user, only: :destroy
 	def new
 		@entry= Entry.new
--- BlogDemo/app/controllers/static_pages_controller.rb
+++ BlogDemo/app/controllers/static_pages_controller.rb
@@ -3,6 +3,8 @@ class StaticPagesController < ApplicationController
   def home
   	if logged_in?
   		@feed_items= current_user.feed.paginate(page: params[:page])
+  	else
+  		@entries= Entry.all
   	end
   end
 
